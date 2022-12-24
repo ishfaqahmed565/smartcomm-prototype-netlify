ee264534a5b9ba8690ab775412f1d5c6efe29f76
ishfaq ahmed <ahmedishfaq737@gmail.com>
2022-12-24 19:01:04 +0600

    FormElems fix test8

--- components/FormElems.vue
+++ components/FormElems.vue
@@ -18,25 +18,24 @@
 			{{ elem.name }} <span class="text-red-300">*</span>
 		</label>
 		<!--select elem-->
-		<div v-if="elem.type === 'select'">
-			<el-select
-				placeholder="Select"
-				:size="size"
-				filterable
-				v-model="elem.value"
-				:multiple="elem.name === 'Tags'"
-				:allow-create="elem.name === 'Tags'"
-				:default-first-option="elem.name === 'Tags'"
+		<el-select
+			placeholder="Select"
+			:size="size"
+			filterable
+			v-model="elem.value"
+			:multiple="elem.name === 'Tags'"
+			:allow-create="elem.name === 'Tags'"
+			:default-first-option="elem.name === 'Tags'"
+			v-if="elem.type === 'select'"
+		>
+			<el-option
+				v-for="item in elem.data"
+				:key="item.value"
+				:label="item.label"
+				:value="item.value"
 			>
-				<el-option
-					v-for="item in elem.data"
-					:key="item.value"
-					:label="item.label"
-					:value="item.value"
-				>
-				</el-option>
-			</el-select>
-		</div>
+			</el-option>
+		</el-select>
 		<!--select element-->
 		<!--input elem-->
 		<el-input
